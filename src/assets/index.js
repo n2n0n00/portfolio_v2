f97112bca1eb25f836050abc7f42bfe85c66a085
AT <athinat690@gmail.com>
2023-12-03 23:03:13 +0000

    projects page done

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,8 @@ import arrow from "./right_arrow.svg";
 import download from "./download.svg";
 import background from "./background.png";
 import cube from "./cube.svg";
+import proj1 from "./proj1.svg";
+import proj2 from "./proj2.svg";
+import proj3 from "./proj3.svg";
 
-export { logo, arrow, download, background, cube };
+export { logo, arrow, download, background, cube, proj1, proj2, proj3 };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { arrow, proj1, proj2, proj3 } from "../assets";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <div className="mx-20 my-[10%] max-md:mt-40 max-md:mx-5">
+        <div className="flex flex-row justify-between items-center max-md:flex-col max-md:justify-center max-md:gap-12">
+          <div className="flex flex-col max-md:items-center">
+            <p className="h4 yellow_text">Portfolio</p>
+            <p className="h1 text-white max-md:text-center w-[60%] max-md:w-full">
+              Selected works 2020 -2022
+            </p>
+          </div>
+          <div className="flex flex-col w-1/5 max-md:w-3/5 max-md:gap-5">
+            <p className="p_reg_white max-md:text-center">
+              Check out more portfolios I&apos;ve worked on
+            </p>
+            <div className="trans_btn w-auto mt-[42px] max-sm:mt-[10px]">
+              <p className="body_md max-sm:text-[12px] text-white">View More</p>
+              <img src={arrow} />
+            </div>
+          </div>
+        </div>
+        <div className="my-20 flex flex-row gap-3 overflow-hidden max-md:flex-col justify-center">
+          <img src={proj1} />
+          <img src={proj2} />
+          <img src={proj3} />
+        </div>
+      </div>
+      <div className="relative w-full mt-[10%]">
+        <div className="projects_green_banner overflow-hidden">
+          <p className="banner m-1 max-sm:m-1">BACKEND</p>
+          <p className="banner m-1 max-sm:m-1">NEXTJS</p>
+          <p className="banner m-1 max-sm:m-1">REACTJS</p>
+          <p className="banner m-1 max-sm:m-1">BACKEND</p>
+          <p className="banner m-1 max-sm:m-1">NEXTJS</p>
+          <p className="banner m-1 max-sm:m-1">REACTJS</p>
+          <p className="banner m-1 max-sm:m-1">BACKEND</p>
+          <p className="banner m-1 max-sm:m-1">NEXTJS</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
