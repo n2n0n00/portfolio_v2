58b5051034a8c8640b319bbdb358c8a4324f0cfc
AT <athinat690@gmail.com>
2023-12-05 20:10:43 +0000

    footer collaboration done

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,5 +6,16 @@ import cube from "./cube.svg";
 import proj1 from "./proj1.svg";
 import proj2 from "./proj2.svg";
 import proj3 from "./proj3.svg";
+import downblack from "./downblack.svg";
 
-export { logo, arrow, download, background, cube, proj1, proj2, proj3 };
+export {
+  logo,
+  arrow,
+  download,
+  background,
+  cube,
+  proj1,
+  proj2,
+  proj3,
+  downblack,
+};
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { cube, downblack } from "../assets/index";
+
+const Collaboration = () => (
+  <div className="flex flex-col items-center gap-10 ml-20 max-md:ml-0">
+    <p className="p_reg yellow_text">Collaboration</p>
+    <h1 className="footer_colab text-white w-full text-center">
+      Let&apos;s talk to Collaboration
+    </h1>
+    <div className="flex flex-row gap-5 ">
+      <div className="button">
+        <div className="text-black items-center justify-center flex flex-row gap-3">
+          {" "}
+          <img src={downblack} />
+          <p>Download CV</p>
+        </div>
+      </div>
+      <div className="trans_btn">
+        <p className="text-white">Hire Me Now</p>
+      </div>
+    </div>
+  </div>
+);
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mx-20 mb-20 mt-[8%] max-md:mt-[15%]">
+      <div className="h-[60vh] max-md:h-full flex flex-row gap-20 w-full items-center justify-center max-md:flex-col mt-[5%]">
+        <div className="h-full justify-end flex flex-col">
+          <img
+            src={cube}
+            width={100}
+            height={100}
+            className="max-md:h-[70px]"
+          />
+        </div>
+        <Collaboration />
+        <div className="h-full justify-start flex flex-col">
+          <img
+            src={cube}
+            width={200}
+            height={200}
+            className="max-md:h-[150px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
