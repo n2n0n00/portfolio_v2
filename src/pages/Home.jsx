cd92b1de97654868be5146c66476c5cc6780c239
AT <athinat690@gmail.com>
2024-05-07 20:02:04 +0100

    home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       </div>
       <div className="mt-[20%] mx-[80px] max-sm:mx-[10px]">
         <div className="flex flex-col items-start justify-start">
-          <h1 className="display text-white">Athena Tzouveleki</h1>
+          <h1 className="display text-white">Athena</h1>
           <p className="p_reg max-md:text-[16px] max-sm:text-[12px] mb-[10%]">
             Hello, my name is Athena, nice to meet you I would like to welcome
             you with my personal portfolio.
